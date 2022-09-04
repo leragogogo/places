47e9025b43cdd6ca2056333b7970971b201849d4
leragogogo <leragogotova@ya.ru>
2022-09-04 16:11:20 +0300

    создание первого экрана

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,56 @@
 import 'package:flutter/material.dart';
-
+import 'package:places/ui/screen/sight_list_screen.dart';
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SightListScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  
-
-  final String title;
+class MyFirstStatelessWidget extends StatelessWidget{
+  int counter = 0;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context){
+    counter++;
+    print("Счетчик StatelessWidget: $counter");
+    return Scaffold(
+      body: Container(
+        child:const Center(
+          child:Text("Hello!")
+        )
+      )
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      
-      _counter++;
-    });
-  }
+class MyFirstStatefulWidget extends StatefulWidget{
 
   @override
-  Widget build(BuildContext context) {
-    
+  State<MyFirstStatefulWidget> createState() => _MyFirstStatefulWidget();  
+}
+class _MyFirstStatefulWidget extends State<MyFirstStatefulWidget>{
+  int counter = 0;
+
+
+  @override
+  Widget build(BuildContext context){
+    counter++;
+    print("Счетчик StatefulWidget: $counter");
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+      body: Container(
+        child:const Center(
+          child:Text("Hello!")
+        )
+      )
     );
   }
 }
