a91e68df7080578db019f364cb3e446ca5497462
Valeriia Gogotova <gogotovavaleria19@gmail.com>
2023-07-13 23:42:41 +0300

    di

---
+++ lib/di.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+
+import 'package:places/data/interactor/filter_interactor.dart';
+import 'package:places/data/interactor/place_interactor.dart';
+import 'package:places/data/interactor/search_interactor.dart';
+import 'package:places/data/interactor/settings_interactor.dart';
+import 'package:places/data_providers/add_sight_provider.dart';
+import 'package:places/data_providers/button_create_provider.dart';
+import 'package:places/data_providers/button_save_provider.dart';
+import 'package:places/data_providers/choosing_category_provider.dart';
+import 'package:places/data_providers/field_empty_provider.dart';
+import 'package:places/data_providers/filter_provider.dart';
+import 'package:places/data_providers/history_provider.dart';
+import 'package:places/data_providers/search_provider.dart';
+import 'package:places/data_providers/visited_provider.dart';
+import 'package:places/data_providers/want_to_visit_provider.dart';
+
+import 'package:places/app.dart';
+
+class DI extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => FiltersProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => SettingsInteractor(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => ChoosingCategoryProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => AddSightProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => ButtonSaveProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => ButtonCreateProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => SearchProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => HistoryProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => FieldEmptyProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => WantToVisitProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => VisitedProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => PlaceInteractor(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => SearchInteractor(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => FilterInteractor(),
+        ),
+      ],
+      child: const App(),
+    );
+  }
+}
